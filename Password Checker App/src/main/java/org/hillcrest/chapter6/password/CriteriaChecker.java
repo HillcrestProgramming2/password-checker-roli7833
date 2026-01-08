188b1452d081f80c180f59d79992b7b540b9ba89
roli7833 <bertaerafa777@gmail.com>
2026-01-08 08:55:09 -0700

    Second Commit hooray

--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
@@ -8,51 +8,87 @@ public class CriteriaChecker {
    // final static String NUMBERS = ("1234567890");
     private static int score = 0;
 
+    private static boolean lengthy;
+    private static boolean hasUpper;
+    private static boolean hasLower;
+    private static boolean hasNumber;
+    private static boolean hasSpecial;
+
     /**
      * Grades Password criteria
      * @param password the inputted password
      * @return the score of the password
      */
     public static int evaluateCriteria(String password) {
-        boolean lengthy = (password.length() >= 8);
-        boolean hasUpper = false;
-        boolean hasLower = false;
-        boolean hasNumber = false;
-        boolean hasSpecial = false;
+        lengthy = (password.length() >= 8);
+        hasUpper = false;
+        hasLower = false;
+        hasNumber = false;
+        hasSpecial = false;
+
 
         //if(password.contains(NUMBERS)) {
         //hasNumber = true;
         //}
-        if (lengthy) {
-            score++;
-        }
+
         for (int i = 0; i < password.length(); i++) {
             char character = (password.charAt(i));
             if (Character.isUpperCase(character)) {
                 hasUpper = true;
-                score++;
             }
 
             character = (password.charAt(i));
             if (Character.isLowerCase(character)) {
                 hasLower = true;
-                score++;
             }
             if (Character.isDigit(password.charAt(i))) {
                 hasNumber = true;
-                score++;
             }
             if (!(Character.isDigit(password.charAt(i))) && (!(Character.isLetter(password.charAt(i))))) {
                 hasSpecial = true;
-                score++;
             }
-
-
         }
+        if (lengthy) {
+            score++;
+        }
+        if(hasUpper) {
+            score++;
+        }
+        if (hasLower) {
+            score++;
+        }
+        if(hasNumber) {
+            score++;
+        }
+        if(hasSpecial) {
+            score++;
+        }
+
+
 
         return score;
 
 
+    }
+
+    public static boolean metLength() {
+        return lengthy;
+    }
+
+    public static boolean metUpper() {
+        return hasUpper;
+    }
+
+    public static boolean metLower() {
+        return hasLower;
+    }
+
+    public static boolean metNumber() {
+        return hasNumber;
+    }
+
+    public static boolean metSpecial() {
+        return hasSpecial;
     }
 
     /**
--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/PasswordCheckerApp.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/PasswordCheckerApp.java
@@ -1,8 +1,23 @@
 package org.hillcrest.chapter6.password;
 
+import java.util.Scanner;
+
+
+
 public class PasswordCheckerApp {
-    //main method goes here
-    //prompt user for password
-    //Check strength
-    //Provide user feedback based on the strength
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter your password!");
+        String password = in.nextLine();
+
+        int score = CriteriaChecker.evaluateCriteria(password);
+        String strength = CriteriaChecker.determineStrength(score);
+        String feedback = FeedbackGenerator.generateFeedback(password);
+        System.out.println("Score: " + score);
+        System.out.println(strength);
+        System.out.println(feedback);
+
+
+    }
 }
